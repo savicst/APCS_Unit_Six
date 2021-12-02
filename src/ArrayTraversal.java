d4765aa38a98f75033d258d1f1cb40772ac3da25
savicst <savicst@ssfs.org>
2021-12-02 14:35:37 -0500

    finished buildArray and countEven; attempted sum28

--- src/ArrayTraversal.java
+++ src/ArrayTraversal.java
@@ -1,17 +1,36 @@
 public class ArrayTraversal {
 
     public static int[] buildArray(int desiredLength) {
-
-        return new int[]{0};
+        int[] newArray = new int[desiredLength];
+        for (int i = 0; i < desiredLength; i++) {
+            newArray[i] = i;
+        }
+        return newArray;
     }
 
     public static int countEven(int[] nums) {
-
-        return 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static boolean sum28(int[] numbers) {
+        int twoSum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == 2) {
+                twoSum += 2;
+            }
+        }
+            if (twoSum == 8) {
+                return true;
+            }
+            else{
+                return false;
+            }
 
-        return true;
     }
 }
