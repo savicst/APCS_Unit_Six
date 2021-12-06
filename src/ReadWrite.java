753330ab35c2427f257c19079fb756400f2bd825
savicst <savicst@ssfs.org>
2021-12-06 08:46:23 -0500

    read and write!

---
+++ src/ReadWrite.java
@@ -0,0 +1,32 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+public class ReadWrite {
+
+    public static String[] read() throws IOException {
+        String [] words = new String[9];
+        Scanner scan = new Scanner(new File("files/readfrom.txt"));
+        int count = 0;
+        while (scan.hasNext()){
+            words[count] = scan.next();
+            count ++;
+        }
+
+        return words;
+    }
+
+    public static void write(String[] arr) throws IOException {
+        FileWriter fw = new FileWriter("files/writeto.txt");
+        for (String word : arr) {
+            fw.write(word + " ");
+        }
+        fw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String[] arr = read();
+        write(arr);
+
+    }
+}
