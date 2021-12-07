7bbd2b50561815e13f11c447a4b2f4c03295b97a
savicst <savicst@ssfs.org>
2021-12-07 09:26:27 -0500

    completed ArrayAlgorithms, unsure why leftShift test does not work, as the expected and actual looj the same to me.

--- src/ArrayAlgorithms.java
+++ src/ArrayAlgorithms.java
@@ -1,18 +1,39 @@
 public class ArrayAlgorithms {
 
     public static boolean checkForAllNegatives(int[] list) {
-
-        return true; // placeholder to avoid errors. You may need to change this.
+        boolean allNegative = true;
+        for (int num : list){
+            if (num>=0){
+                allNegative = false;
+            }
+        }
+        return allNegative;
     }
 
     public static boolean hasDupes(int[] list) {
-
-        return true; // placeholder to avoid errors. You may need to change this.
+        boolean anyDupes = false;
+        int count=-1;
+        for (int position : list){
+            count++;
+            for (int i=0; i<count; i++){
+                if (position == list[i])
+                    anyDupes= true;
+            }
+        }
+        return anyDupes; // placeholder to avoid errors. You may need to change this.
     }
 
 
     public static int[] leftShift(int[] nums, int shift) {
-
-        return new int[] {}; // placeholder to avoid errors. You may need to change this.
+        int[] newArr = new int[nums.length+ shift];
+        int index = 0;
+        for(int num : nums){
+            newArr[index]= num;
+            index++;
+        }
+        for(int i= index+1; i<newArr.length; i++){
+            newArr[i] = 0;
+        }
+        return newArr; // placeholder to avoid errors. You may need to change this.
     }
 }
